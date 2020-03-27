15170a87700982ee40c9f016b70a9c3954e08b23
Trip4077 <bj2336@email.vccs.edu>
2020-03-27 17:46:00 -0400

    knex configuration

---
+++ knexfile.js
@@ -0,0 +1,34 @@
+// Knex Configuration for Database Connections
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    
+    connection: {
+      filename: "./data/db.sqlite3"
+    },
+
+    useNullAsDefault: true,
+    migrations: { directory: "./data/migrations" },
+    seeds: { directory: "./data/seeds" }
+  },
+
+  testing: {
+    client: "sqlite3",
+
+    connection: {
+      filename: "./data/testDb.sqlite3"
+    },
+
+    useNullAsDefault: true,
+    migrations: { directory: "./data/migrations" },
+    seeds: { directory: "./data/seeds" }
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: { directory: "./data/migrations" },
+    seeds: { directory: "./data/seeds" }
+  }
+};
