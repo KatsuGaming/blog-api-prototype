2682c774e34972839532bc6d402218cd4f070808
Trip4077 <bj2336@email.vccs.edu>
2020-03-31 13:26:50 -0400

    Blog Routes

---
+++ routes/blog_routes.js
@@ -0,0 +1,65 @@
+const router = require( 'express' ).Router();
+const Blogs = require( '../models/blog_model' );
+
+router.get( '/', async ( _, res ) => {
+    try {
+        const blogs = await Blogs.getAll();
+
+        res.status( 200 ).json( blogs );
+    } catch(err) {
+        res.status( 500 ).json({ message: "Error Retrieving Blogs", error: err });
+    }
+});
+
+router.get( '/:id', async ( req, res ) => {
+    try {
+        const { id } = req.params;
+
+        const blog = await Blogs.getBy({ id });
+    
+        res.status( 200 ).json( blog );
+    } catch(err) {
+        res.status( 500 ).json({ message: "Error Retrieving Blog", error: err });
+    }
+});
+
+router.post( '/', async ( req, res ) => {
+    try {
+        const newData = req.body;
+
+        newData.created_at = new Date().toLocaleDateString();
+
+        const newBlog = await Blogs.add( newData );
+
+        res.status( 201 ).json( newBlog );
+    } catch(err) {
+        res.status( 500 ).json({ message: "Error Adding Blog", error: err });
+    }
+});
+
+router.put( '/:id', async ( req, res ) => {
+    try {
+        const updateData = req.body;
+        const { id } = req.params;
+
+        const updatedBlog = await Blogs.update( id, updateData );
+
+        res.status( 200 ).json( updatedBlog );
+    } catch(err) {
+        res.status( 500 ).json({ message: "Error Updating Blog", error: err });
+    }
+});
+
+router.delete( '/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const removedBlog = await Blogs.remove( id );
+
+        res.status( 200 ).json( removedBlog );
+    } catch(err) {
+        res.status( 500 ).json({ message: "Error Removing Blog", error: err });
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const server = require( 'express' )();
 const middleware = require( './middleware' );
+const routes = require( './routes' );
 
 server.use( middleware );
+server.use( '/api', routes );
 
 server.get('/', (_, res) => res.status(200).json({ message: "Server Running..." }));
 
