41fea197090dca5d16a45a96ee18b10a059f0e04
Trip4077 <bj2336@email.vccs.edu>
2020-03-31 12:57:48 -0400

    Middleware Abstraction

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-const express = require( 'express' );
+const server = require( 'express' )();
+const middleware = require( './middleware' );
 
-const helmet = require( 'helmet' );
-const cors = require( 'cors' );
-const logger = require( 'morgan' );
+server.use( middleware );
 
-const server = express();
-
-server.use(
-    express.json(),
-    helmet(),
-    cors(),
-    logger( 'dev' )
-);
-
-server.get('/', (_, res) => res.status(200).json({ message: "Server Running..." }))
+server.get('/', (_, res) => res.status(200).json({ message: "Server Running..." }));
 
 module.exports = server;
